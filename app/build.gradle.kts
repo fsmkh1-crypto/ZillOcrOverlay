8f70006a19ef5fc942dc3f0f1cb84d301c604cf0
fsmkh1-crypto <fsmkh1@gmail.com>
2026-08-23 14:15:46 +0900

    Bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "kr.co.zillocr.overlay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.0"
+        versionCode = 7
+        versionName = "0.3.1"
     }
 
     buildTypes {
