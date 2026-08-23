14fba736a6f5d1d16afde95e80719f90adc7059e
fsmkh1-crypto <fsmkh1@gmail.com>
2026-08-23 17:54:42 +0900

    Bump version to 0.5.0-alpha5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "kr.co.zillocr.overlay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.5.0-alpha4"
+        versionCode = 18
+        versionName = "0.5.0-alpha5"
     }
 
     buildTypes {
