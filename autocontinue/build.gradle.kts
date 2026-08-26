7b89673d20cec123d91624ed242266a22615bcdb
fsmkh1-crypto <fsmkh1@gmail.com>
2026-08-26 13:49:45 +0900

    Add ChatGPT automation app module

---
+++ autocontinue/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.fsmkh1.chatgptautocontinue"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.fsmkh1.chatgptautocontinue"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
